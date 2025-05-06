d46df10026a0fc24d0aeac05adda2b950af7d121
linzengyi <xanxus.v.zero@gmail.com>
2025-05-06 17:19:50 +0800

    settings.py修正設定v4

--- ecart/settings.py
+++ ecart/settings.py
@@ -136,7 +136,7 @@ if DEBUG:
 else:
     print('aaa')
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
