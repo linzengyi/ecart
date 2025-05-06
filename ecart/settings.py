de6dacc053328c53c507a1a69a549826ea19196d
linzengyi <xanxus.v.zero@gmail.com>
2025-05-06 17:35:43 +0800

    settings.py修正設定v5

--- ecart/settings.py
+++ ecart/settings.py
@@ -128,12 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [ 
+    BASE_DIR / 'static', 
+]
 
-if DEBUG:
-    STATICFILES_DIRS = [ 
-        BASE_DIR / 'static', 
-    ]
-else:
+if not DEBUG:
     print('aaa')
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
